490faa6d15f3706e8fe5557f64aaf15742728d16
6LYTH3 <ST4R13UCK@gmail.com>
2015-04-25 14:25:47 +0700

    Make a simple template

--- app.go
+++ app.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/gorilla/mux"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
+	http.Handle("/", http.FileServer(http.Dir("webapp")))
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	b, _ := json.Marshal(map[string]string{"text": "hello"})
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(b)
+	log.Printf("Go Baby go ...")
+	err := http.ListenAndServe(":8080", nil)
+	fmt.Println(err.Error())
 }
