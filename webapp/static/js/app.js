bbf6a896dc8ec07933b9c7c31ee5b2cca880a78e
6LYTH3 <ST4R13UCK@gmail.com>
2015-04-27 18:00:51 +0700

    Hacking: augularJS

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -1,1 +1,24 @@
 // app.js
+
+var app = angular.module('index', []);
+
+app.controller('OrderCtrl', ['$scope','$http',
+        function($scope, $http) {
+            $scope.working = false;
+
+            var logError = function(data, status) {
+                console.log('Code '+status+': '+data)
+                $scope.working = false;
+            };
+                
+
+            $scope.search = function() {
+            $scope.working = true;
+            $http.get('/iElsa/workorder/'+$scope.workorder).
+                error(logError).
+                success(function(data) { 
+                    $scope.orderlist = data.Workorder; 
+                    $scope.working = false;
+                });
+            };
+}]);
